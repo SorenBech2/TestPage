d778b67e7a526df65129fec547254fde30772ac7
Søren Bech <soren-bech@outlook.com>
2024-08-29 21:02:44 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace TestPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GoToSecondPage_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"{nameof(SecondPage)}", true);
+        }
+    }
+
+}
---
+++ SecondPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace TestPage;
+
+public partial class SecondPage : ContentPage
+{
+	public SecondPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..", true);
+    }
+}
